5ae1b7e8a9fb8bccf6e328af1ea1d4ef9cb4b4af
rupa <rupa@karmamgmt.com>
2025-08-25 16:06:34 +0530

    create complaince mster dashboard

--- src/page/DashboardPage.jsx
+++ src/page/DashboardPage.jsx
@@ -7,6 +7,7 @@ import StatsCards from '../component/StatsCards';
 import Donut from '../component/charts/Donut';
 import StackedBar from '../component/charts/StackedBar';
 import NotificationPage from '../component/notification/NotificationPage';
+import ComplianceMasterDashboard from './ComplianceMasterDashboard';
 
 const DashboardPage = () => {
   const [isModalOpen, setIsModalOpen] = useState(false);
@@ -67,14 +68,8 @@ const DashboardPage = () => {
   const crudTitle = "Upload File"
   return (
     <div>
-      <Modal crudForm={crudForm} crudTitle={crudTitle} isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen} closeModal={closeModal} />
-      {/* <div className=' d-flex justify-content-end'>
-        <button className='w-auto upload_btn' onClick={openModal}>Upload File</button>
-      </div> */}
+      {/* <Modal crudForm={crudForm} crudTitle={crudTitle} isModalOpen={isModalOpen} setIsModalOpen={setIsModalOpen} closeModal={closeModal} />
       <div className='mb-4 d-flex'>
-        {/* <span className='v-line me-1 mt-1'></span>
-        <span className='v-line me-1 mt-1'></span>
-        <span className='v-line me-1 mt-1'></span> */}
         <span className='dashboard-heading ms-2'>Dashboard</span>
       </div>
       <div>
@@ -87,58 +82,10 @@ const DashboardPage = () => {
         <div className='stat-card '>
           <StackedBar />
         </div>
-      </div>
-      {/* <div className='row'>
-        <div className='col-4 col-md-4'>
-          <div className='white-card mb-3'>
-            <div className='p-3'>
-              <div className='dashboard-card-header-text'>Billing & Margin</div>
-              <div className='dashboard-card-sub-header-text'>Total Billed</div>
-              <div className='dashboard-card-header-text'><span>₹</span>&nbsp;<span>2,950,000</span></div>
-              <div className='dashboard-card-sub-header-text'>Margin</div>
-              <div className='dashboard-card-header-text'>630,000</div>
-
-            </div>
-
-          </div>
-          <div className='white-card mb-3'>
-            <div className='p-3'>
-
-              <div className='dashboard-card-header-text'>Outstanding Ageing Summary</div>
-              <div className='dashboard-card-sub-header-text d-lg-flex d-md-flex justify-content-between'><span className='progress-text1'>Good</span>
-                <span className="progress w-50 mt-1">
-                  <div className="progress-bar" role="progressbar" style={{ width: '25%' }} aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
-                </span><span className='progress-text'>22,000</span>
-              </div>
-
-              <div className='dashboard-card-header-text'><span>₹</span>&nbsp;<span>2,950,000</span></div>
-              <div className='dashboard-card-sub-header-text'>Margin</div>
-              <div className='dashboard-card-header-text'>630,000</div>
-            </div>
-
-          </div>
-          <div className='white-card mb-3'>
-          </div>
-        </div>
-
-        <div className='col-4 col-md-4'>
-          <div className='white-card mb-3'>
-          </div>
-          <div className='white-card mb-3'>
-          </div>
-          <div className='white-card mb-3'>
-          </div>
-        </div>
-
-        <div className='col-4 col-md-4'>
-          <div className='white-card mb-3'>
-          </div>
-          <div className='white-card mb-3'>
-          </div>
-          <div className='white-card mb-3'>
-          </div>
-        </div>
       </div> */}
+      <div>
+        <ComplianceMasterDashboard/>
+      </div>
     </div>
   )
 }
