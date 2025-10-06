58ec194f8faf566620de1bde5ab57491fff488a5
rupajha <rupa@karmamgmt.com>
2025-10-06 10:50:39 +0530

    remove folder from style

--- src/style/route/ProtectedRoute.jsx
+++
@@ -1,7 +0,0 @@
-import { Navigate, Outlet } from 'react-router-dom';
-
-const ProtectedRoute = ({ isAuthenticated }) => {
-  return isAuthenticated ? <Outlet /> : <Navigate to="/" />;
-};
-
-export default ProtectedRoute;
